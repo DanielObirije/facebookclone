335c4234d8855429f813191822e2fae69806866c
Daniel Obirije <obirijedaniel@gmail.com>
2023-09-12 15:10:50 +0200

    second commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,19 +2,15 @@
 import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from 'firebase/auth'
 import {getFirestore} from  'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZU3waT7zBzGwoUSCMAIQpzuwrFueqmwo",
-  authDomain: "facebook-clone-project-c4d11.firebaseapp.com",
-  projectId: "facebook-clone-project-c4d11",
-  storageBucket: "facebook-clone-project-c4d11.appspot.com",
-  messagingSenderId: "643539265360",
-  appId: "1:643539265360:web:3b9895d0bbd506c2dfee55"
+  apiKey: "AIzaSyD0m7hC6auHY8mTsRimfQw6gTVfedISK2o",
+  authDomain: "facebook-clone-8c232.firebaseapp.com",
+  projectId: "facebook-clone-8c232",
+  storageBucket: "facebook-clone-8c232.appspot.com",
+  messagingSenderId: "6675240022",
+  appId: "1:6675240022:web:d8ce37e26214d831f197f7"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
